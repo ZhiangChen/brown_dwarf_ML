c5fe30052b550991084f0a3fd496bce0bff51132
ZhiangChen <zxc251@case.edu>
2019-11-24 18:31:35 -0700

    added a FCL model

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ Nov 24, 2019
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
+import pandas as pd
 from data import *
 
 class FCN(object):
